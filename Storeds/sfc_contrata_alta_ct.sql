6b1b272aef037c6a361f9126b554f82610089952
ldvalle <lukrecio01@hotmail.com>
2021-05-04 17:00:39 -0300

    Cambio Titularidad

--- Storeds/sfc_contrata_alta_ct.sql
+++ Storeds/sfc_contrata_alta_ct.sql
@@ -17,6 +17,17 @@ DEFINE retNroOrden      integer;
 DEFINE auxCod   smallint;
 DEFINE auxDesc  char(50);
 DEFINE proc_pend    char(20);
+DEFINE sRolOrigen   like rol.rol;
+DEFINE sAreaOrigen  like rol.area;
+DEFINE nrows        integer;
+
+DEFINE sql_err              INTEGER;
+DEFINE isam_err             INTEGER;
+DEFINE error_info           CHAR(100);
+
+    ON EXCEPTION SET sql_err, isam_err, error_info
+        RETURN 1, 'sfc_contrata_alta_ct. sqlErr '  || to_char(sql_err) || ' isamErr ' || to_char(isam_err) || ' ' || error_info;
+    END EXCEPTION;
 
     -- Verificar Cliente viejo y procedimientos pendientes
     EXECUTE PROCEDURE sfc_verif_clteviejo(nroClienteVjo) INTO auxCod, auxDesc, proc_pend;
@@ -24,6 +35,21 @@ DEFINE proc_pend    char(20);
     IF auxCod != 0 THEN
         RETURN auxCod, auxDesc;
     END IF; 
+    
+    -- Rol y Area Origen
+    LET nrows=0;
+    SELECT s.rol, r.area INTO sRolOrigen, sAreaOrigen 
+    FROM cliente c, sucur_centro_op o, sfc_roles s, rol r
+    WHERE c.numero_cliente = nroClienteVjo
+    AND o.cod_centro_op = c.sucursal 
+    AND s.procedimiento = 'INC' 
+    AND s.sucursal = o.cod_sucur
+    AND r.rol = s.rol;
+
+	LET nrows = DBINFO('sqlca.sqlerrd2');
+	IF nrows = 0 THEN
+		RETURN 1, 'No se encontro Rol/Area Origen.';
+	END IF;
     
     -- Mueve Clientes  
     EXECUTE PROCEDURE sfc_mover_clientes(nroClienteVjo, nroClienteNvo, nroSolicitud) INTO auxCod, auxDesc;
@@ -39,6 +65,13 @@ DEFINE proc_pend    char(20);
         RETURN auxCod, auxDesc;
     END IF; 
 
+    -- Orden CamTit
+    EXECUTE PROCEDURE sfc_orden_camtit(nroClienteNvo, nroMensaje, sRolOrigen, sAreaOrigen) INTO auxCod, auxDesc;
+
+    IF auxCod != 0 THEN
+        RETURN auxCod, auxDesc;
+    END IF; 
+    
 	RETURN 0, 'OK';
 
 END PROCEDURE;
